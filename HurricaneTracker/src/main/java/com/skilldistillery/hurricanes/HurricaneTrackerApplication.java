7a71d6118913e0b1f2a5be8346ac37a2bb123a57
diegoescutia <escutia.diego@gmail.com>
2022-08-12 20:35:31 -0400

    linked projects

---
+++ HurricaneTracker/src/main/java/com/skilldistillery/hurricanes/HurricaneTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hurricanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurricaneTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurricaneTrackerApplication.class, args);
+	}
+
+}
